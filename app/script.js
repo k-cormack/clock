509980ae1a93a6f058f79dc47715fccedf7be808
k-cormack <k.cormack@gmail.com>
2018-07-26 16:38:03 -0600

    AM/PM added

--- app/script.js
+++ app/script.js
@@ -4,10 +4,25 @@ $(document).ready(function() {
         var hours = currentTime.getHours();
         var minutes = currentTime.getMinutes();
         var seconds = currentTime.getSeconds();
+        var meridiem = "AM";
+        if (hours > 12) {
+            hours = hours - 12;
+            meridiem = "PM"
+        }
+        if (hours === 0) {
+            hours = 12;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (seconds > 50) {
+            seconds = "Buzzzz"
+        }
         var clockDiv = document.getElementById('clock');
-        clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
-    }
+        clockDiv.innerText = hours + ":" + minutes + ":" + seconds + meridiem;
+    }    
 
     displayTime();
+    setInterval(displayTime, 1000)
 
 });
